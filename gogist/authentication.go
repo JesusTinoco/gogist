c2b2773ba2233f75416dc39fc561ef2d4fb7420e
Jesus Tinoco <jesus.rodriguez.tinoco@gmail.com>
2016-04-13 23:52:11 +0200

    Add message per each code in createNewToken

--- gogist/authentication.go
+++ gogist/authentication.go
@@ -3,12 +3,20 @@ package gogist
 import (
 	"fmt"
 	"gogist/client"
+	"strings"
 )
 
 const url string = "https://api.github.com/authorizations"
 
 func CreateNewToken(username string, password string) {
-	code, body := client.Post(url, `{"note": "Gogist2 tool", "scope": "gists"}`, username, password)
-	fmt.Println(code)
-	fmt.Println(body)
+	code, _ := client.Post(url, `{"note": "Gogist2 tool", "scope": "gists"}`, username, password)
+
+	if strings.Contains(code, "422") {
+		panic("Token already exist. Remove it before continuing")
+	} else if strings.Contains(code, "403") {
+		panic("Failed to access to the URL")
+	} else if strings.Contains(code, "201") {
+		fmt.Println("Token created successfully. It will be safe at ~/gogist")
+	}
+
 }
