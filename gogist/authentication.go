1a992605d612830914c72b01915cf17e1b9d1c47
Jesus Tinoco <jesus.rodriguez.tinoco@gmail.com>
2016-04-14 20:47:12 +0200

    Parsing JSON response and writing the token into a file

--- gogist/authentication.go
+++ gogist/authentication.go
@@ -1,22 +1,58 @@
 package gogist
 
 import (
+	"encoding/json"
 	"fmt"
 	"gogist/client"
+	"os"
 	"strings"
 )
 
 const url string = "https://api.github.com/authorizations"
 
+type TokenJSON struct {
+	Id  int    `json:"id"`
+	Url string `json:"url"`
+	//App            []App  `json:"app"`
+	Token          string `json:"token"`
+	HashedToken    string `json:"hashed_token"`
+	TokenLastEight string `json:"token_last_eight"`
+	Note           string `json:"note"`
+	NoteUrl        string `json:"note_url"`
+	CreatedAt      string `json:"created_at"`
+	UpdatedAt      string `json:"updated_at"`
+	Scope          string `json:"scope"`
+	Fingerprint    string `json:"fingerprint"`
+}
+
+type App struct {
+	Name     string `json:"name"`
+	Url      string `json:"url"`
+	ClientId string `json:"client_id"`
+}
+
 func CreateNewToken(username string, password string) {
-	code, _ := client.Post(url, `{"note": "Gogist2 tool", "scope": "gists"}`, username, password)
+
+	code, body := client.Post(url, `{"note": "Gogist2 tool", "scope": "gists"}`, username, password)
 
 	if strings.Contains(code, "422") {
 		panic("Token already exist. Remove it before continuing")
 	} else if strings.Contains(code, "403") {
-		panic("Failed to access to the URL")
+		panic("Failed accessing to the URL")
 	} else if strings.Contains(code, "201") {
 		fmt.Println("Token created successfully. It will be safe at ~/gogist")
 	}
 
+	var tokenJson TokenJSON
+	err := json.Unmarshal([]byte(body), &tokenJson)
+	if err != nil {
+		panic(err)
+	}
+
+	file, err := os.Create(".gogist")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	file.WriteString(tokenJson.Token)
 }
